5184ef516d80925210970954cefdeaab061b1996
Amit Natani <eng.amitnatani1993@gmail.com>
2018-11-13 15:36:47 +0530

    Redirect based on environment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent implements OnInit {
   email: String = null;
 
   redirect_uri = environment.REDIRECT_URI;
+  api_base_uri = environment.API_BASE_URL;
 
   constructor(private http: HttpClient) { }
 
@@ -22,7 +23,7 @@ export class LoginComponent implements OnInit {
   login(): void {
     // window.location.href = "http://localhost:8080/login?email=amit.natani%40metacube.com&client_id=222"
     // window.location.href = `http://dev-accounts.agilestructure.in/sessions/new?state=${this.redirect_uri}&email=${this.email}`;
-    window.location.href = `http://localhost:3000/sessions/new?email=${this.email}`;
+    window.location.href = `${this.api_base_uri}/sessions/new?email=${this.email}`;
   }
 
 }
